d598af2895b115a4f5b2ed7fd655ef33d7367c9c
ylagorebollar <ylagorebollar@danielcastelao.org>
2016-11-25 12:36:36 +0100

    Dos fragments en un layout

--- app/src/main/java/com/example/yasmin/intent/MainActivityFragment.java
+++ app/src/main/java/com/example/yasmin/intent/MainActivityFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,34 +15,37 @@ import android.widget.Button;
 public class MainActivityFragment extends Fragment {
 
     Button btnHi;
-    View fragmentoPricipal;
+    View fragmentoPrincipal;
+    View fragmentoSecundario;
+    boolean tipoPantalla;
 
     public MainActivityFragment() {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        fragmentoPricipal = inflater.inflate(R.layout.fragment_main, container, false);
+        fragmentoPrincipal = inflater.inflate(R.layout.fragment_main, container, false);
 
-        btnHi = (Button)fragmentoPricipal.findViewById(R.id.btnHi);
+        btnHi = (Button) fragmentoPrincipal.findViewById(R.id.btnHi);
         btnHi.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                if(fragmentoPricipal.findViewById(R.id.fragment)==null){
+                if(fragmentoPrincipal.findViewById(R.id.fragment)==null){
                     Intent intentPrincipal = new Intent(getActivity(), SecondActivity.class);
-                startActivity(intentPrincipal);
+                    startActivity(intentPrincipal);
                 }
-                if(fragmentoPricipal.findViewById(R.id.fragmentLand)==null) {
-                    Intent intentSecundario = new Intent(getActivity(), SecondActivity.class);
-                    startActivity(intentSecundario);
+                if(fragmentoPrincipal.findViewById(R.id.fragmentLand)==null) {
+
+                    Toast toast = Toast.makeText(getApplicationContext(), "Abre una ventana", Toast.LENGTH_SHORT);
+                    toast.show();
                 }
             }
         });
-        return fragmentoPricipal;
+        return fragmentoPrincipal;
     }
 
 
