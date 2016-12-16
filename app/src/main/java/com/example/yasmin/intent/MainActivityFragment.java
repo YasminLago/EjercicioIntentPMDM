51c65e89988a4c25d541edbf24dff5b74382d121
YasminLago <ylagorebollar@danielcastelao.org>
2016-12-16 14:40:15 +0100

    Update MainActivityFragment.java

--- app/src/main/java/com/example/yasmin/intent/MainActivityFragment.java
+++ app/src/main/java/com/example/yasmin/intent/MainActivityFragment.java
@@ -17,10 +17,17 @@ public class MainActivityFragment extends Fragment{
 
     Comunicador comunicador;
 
+    /**
+     * Se crea la interfaz que conectará los dos fragments
+     */
     public interface Comunicador{
         public void mensaje(String texto);
     }
 
+    /**
+     * Conecta el fragmento con la actividad
+     * @param contexto
+     */
     public void onAttach(Context contexto){
         super.onAttach(contexto);
         comunicador = (Comunicador)contexto;
@@ -44,6 +51,9 @@ public class MainActivityFragment extends Fragment{
 
             @Override
             public void onClick(View v) {
+                // Devuelve true si la propiedad existe y es igual a true.
+                // Por lo tanto, se indica que si la pantalla está en modo landscape
+                // se ejecutará el codigo contenido en el if
                 if(getResources().getBoolean(R.bool.modoDosPantallas)){
                     //Toast toast = Toast.makeText(getActivity(),"Estoy en Landscape", Toast.LENGTH_SHORT);
                     //toast.show();
@@ -52,9 +62,9 @@ public class MainActivityFragment extends Fragment{
                 }else {
                    // Intent intentPrincipal = new Intent(getActivity(), SecondActivity.class);
                    // startActivity(intentPrincipal);
-                    Intent intentPrincipal = new Intent(getActivity(),SecondActivity.class);
-                    intentPrincipal.putExtra(MainActivityFragment.FRASE, "Adiós mundo");
-                    startActivity(intentPrincipal);
+                    Intent intentPrincipal = new Intent(getActivity(),SecondActivity.class); //Se crea el intent que llamará a la segunda actividad
+                    intentPrincipal.putExtra(MainActivityFragment.FRASE, "Adiós mundo"); // Se envía el mensaje que aparecerá en el TextView
+                    startActivity(intentPrincipal); //Se llama a la segunda actividad
                 }
             }
         });
