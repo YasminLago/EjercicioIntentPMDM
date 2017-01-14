ebab19b857e3028d8abdfd5c6ccd444fc8004e99
Yasmin <ylagorebollar@danielcastelao.org>
2017-01-14 21:26:23 +0100

    Acceso a recursos layout y definicion del metodo setOnClickListener Envio de datos a MainActivityFragment a traves de metodo setResult

--- app/src/main/java/com/example/yasmin/intent/SecondActivity.java
+++ app/src/main/java/com/example/yasmin/intent/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.example.yasmin.intent;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,12 +8,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
     TextView texto;
     String nombre;
+    Button devolver;
+    EditText txtDevuelto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +27,18 @@ public class SecondActivity extends AppCompatActivity {
                 nombre = intent.getStringExtra(MainActivityFragment.FRASE); // Se guarda el mensaje en un String
                 texto = (TextView) findViewById(R.id.text);
                 texto.setText(nombre); //Se envía el mensaje al TextView
+
+                txtDevuelto = (EditText) findViewById(R.id.textoDevolver);
+                devolver = (Button) findViewById(R.id.btnDevolverTxt);
+                devolver.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String textoEscrito = txtDevuelto.getText().toString();
+                        Intent retorna =  new Intent();
+                        retorna.putExtra("TEXTO", textoEscrito);
+                        setResult(RESULT_OK, retorna);
+                        finish();
+                    }
+                });
     }
 }
