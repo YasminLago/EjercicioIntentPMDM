1d94c5311dceb8f4407931dd3df876544cf7343a
Yasmin <ylagorebollar@danielcastelao.org>
2017-01-14 21:23:10 +0100

    Añadido texto hint en editText Definido metodo onActivityResult + comentarios del metodo

--- app/src/main/java/com/example/yasmin/intent/MainActivityFragment.java
+++ app/src/main/java/com/example/yasmin/intent/MainActivityFragment.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -36,7 +39,9 @@ public class MainActivityFragment extends Fragment{
 
     Button btnHi;
     View fragmentoPrincipal;
+    TextView textoRecibido;
     final static String FRASE = "Hi world!";
+    final static int MENSAJE = 1; //Identifica la petición realizada
 
     public MainActivityFragment(){
     }
@@ -46,6 +51,7 @@ public class MainActivityFragment extends Fragment{
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         fragmentoPrincipal = inflater.inflate(R.layout.fragment_main, container, false);
+        textoRecibido = (TextView) fragmentoPrincipal.findViewById(R.id.textoRecibido);
         btnHi = (Button) fragmentoPrincipal.findViewById(R.id.btnHi);
         btnHi.setOnClickListener(new View.OnClickListener(){
 
@@ -62,13 +68,34 @@ public class MainActivityFragment extends Fragment{
                 }else {
                    // Intent intentPrincipal = new Intent(getActivity(), SecondActivity.class);
                    // startActivity(intentPrincipal);
-                    Intent intentPrincipal = new Intent(getActivity(),SecondActivity.class); //Se crea el intent que llamará a la segunda actividad
-                    intentPrincipal.putExtra(MainActivityFragment.FRASE, "Adiós mundo"); // Se envía el mensaje que aparecerá en el TextView
-                    startActivity(intentPrincipal); //Se llama a la segunda actividad
+                        Intent intentPrincipal = new Intent(getActivity(),SecondActivity.class); //Se crea el intent que llamará a la segunda actividad
+                        intentPrincipal.putExtra(MainActivityFragment.FRASE, "Adiós mundo"); // Se envía el mensaje que aparecerá en el TextView
+                        startActivityForResult(intentPrincipal, MENSAJE); //Se llama a la segunda actividad esperando un resultado
+
+
                 }
             }
         });
         return fragmentoPrincipal;
+    }
+
+    /**
+     * Permite usar los datos obtenidos de la actividad que ha sido llamada (SecondActivity)
+     * @param requestCode Indentifica que Activity está proporcionando los resultados
+     * @param resultCode Valor retornado por la Activity que ha sido llamada (SecondActivity)
+     * @param data Retorna datos a la Activity que ha realizado la llamada (MAinActivityFragment)
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == MENSAJE && resultCode == RESULT_OK){
+            String resultado = String.valueOf(RESULT_OK);
+            String textoDevuelto = data.getStringExtra("TEXTO");
+            Log.d("blblabla","TEXTO SALIDA"+ "MENSAJE"+ MENSAJE + "requestCode"+requestCode);
+            textoRecibido.setText(textoDevuelto);
+
+
+        }
     }
 
 
@@ -80,6 +107,4 @@ public class MainActivityFragment extends Fragment{
 
 
 
-
-
-}
+    }
